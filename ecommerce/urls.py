45398cd838b9340e47dc55d1f0459e20a2955ffd
MeJerryD <dkimurik@gmail.com>
2023-03-15 18:40:24 +0200

    added stuff from category

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -6,7 +6,7 @@ Examples:
 Function views
     1. Add an import:  from my_app import views
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
+Class-based viewss
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
@@ -15,8 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+from shop import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('<slug:category_slug>', views.home, name='products_by_category'),
     path('', include('shop.urls')),
 ]
+
+if settings.DEBUG: # обновляем url в дебаг моде (в settings)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
